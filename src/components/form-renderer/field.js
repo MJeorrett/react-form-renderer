edb1f6ea4e76a6cf05bc9eaee25a15428a225540
Matthew Jeorrett <matthewjeorrett@gmail.com>
2018-05-06 20:42:14 +0100

    field: add visual feedback for 'required' validation

--- src/components/form-renderer/field.js
+++ src/components/form-renderer/field.js
@@ -1,26 +1,72 @@
 import React, { Component } from 'react';
 import { PropTypes } from 'prop-types';
-import { Form } from 'semantic-ui-react';
+import { Form, Label } from 'semantic-ui-react';
 
+import './field.css';
 import { field as fieldPropType } from './propTypes';
 import { VALIDATIONS } from './validations';
 
 export class Field extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            value: "",
+            isRequired: false,
+            isError: false,
+            pristine: true
+        };
+    }
+
     handleChange = (event) => {
-        this.props.onUpdate(event.target.value);
+        const value = event.target.value;
+
+        this.setState({
+            value
+        });
+
+        this.props.onUpdate(value);
+    }
+
+    handleOnBlur = () => {
+        const { isRequired, value } = this.state;
+
+        this.setState({
+            pristine: false,
+            isError: isRequired && !value
+        });
+    }
+
+    static getDerivedStateFromProps(nextProps, prevState) {
+        const { field } = nextProps;
+        const { pristine, value } = prevState;
+
+        const isRequired = field.validations && field.validations.includes(VALIDATIONS.REQUIRED);
+        const isError = isRequired && !pristine && !value;
+
+        return {
+            isRequired,
+            isError
+        };
     }
 
     render() {
         const { field } = this.props;
-        const required = field.validations && field.validations.includes(VALIDATIONS.REQUIRED);
+        const { isRequired, isError } = this.state;
+
         return (
+            <div>
             <Form.Input
                 fluid
+                error={isError}
                 label={field.title}
                 placeholder={field.title}
                 onChange={this.handleChange}
-                required={required}>
+                onBlur={this.handleOnBlur}
+                required={isRequired}>
             </Form.Input>
+            <Label className={isError ? '' : 'visibilityHidden'} color="red">Please enter a value.</Label>
+            </div>
         )
     }
 }
