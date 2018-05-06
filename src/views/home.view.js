85da07ebec66a0a010091c2f58a600e9bc5d5fb5
Matthew Jeorrett <Matthew@wallet.services>
2018-05-06 13:39:49 +0100

    home.view: wrap in container

--- src/views/home.view.js
+++ src/views/home.view.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import { Container, Header } from 'semantic-ui-react';
 
 export class HomeView extends Component {
     render() {
         return (
-            <Header as="h1">Welcome to Form Builder</Header>
+            <Container>
+                <Header as="h1">Welcome to Form Builder</Header>
+            </Container>
         );
     }
 }
